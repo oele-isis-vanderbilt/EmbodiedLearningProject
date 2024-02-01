05316fcaffa6941abcdd8cad6e4af87a1943d492
edavalosanaya <davalosaeduardo@gmail.com>
2024-02-01 14:36:10 -0600

    Updated the reid script to improve performance.

--- scripts/gaze/copy_file_script.py
+++ scripts/gaze/copy_file_script.py
@@ -35,8 +35,13 @@ def copy_random_pngs(src_dir: pathlib.Path, dst_dir: pathlib.Path, N: int):
         print(f"Copied {file} to {dst_dir}")
 
 # Example usage
-src_dir = DATA_DIR / 'reid' / 'cropped_faces' / 'd1g1'
-dst_dir = DATA_DIR / 'reid' / 'cropped_faces' / 'd1g1_sample'
+
+# src_dir = DATA_DIR / 'reid' / 'cropped_faces' / 'd1g1'
+# dst_dir = DATA_DIR / 'reid' / 'cropped_faces' / 'd1g1_sample'
+
+src_dir = DATA_DIR / 'reid' / 'cropped_faces' / 'd1g2'
+dst_dir = DATA_DIR / 'reid' / 'cropped_faces' / 'd1g2_sample'
+
 N = 100 # Number of random images you want to copy
 
 copy_random_pngs(src_dir, dst_dir, N)
